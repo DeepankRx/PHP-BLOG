c8017caad7ea4e5db9e4f49bf5fc1d0263b596e7
Deepank <deepankpushpad10@gmail.com>
2022-01-18 01:35:20 +0530

    first commit

---
+++ views/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Programming Hub</title>
+  </head>
+  <body>
+    
+    <nav>
+      <h1 ><a class="title" href="<?php echo $_SERVER['PHP_SELF'] ?>" >Programming Hub</a></h1>
+      <ul>
+        <li><a class="nav-element" href="<?php echo $_SERVER['PHP_SELF'] ?>" >Home</a></li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+      
+<div class="home-container">
+    <div class="header-content">
+        <h1 class="welcome-line"> Welcome To <span class="title">Programming Hub</span></h1>
+        <p class="welcome-text">Confused on which course to take? I have got you covered. Browse courses and find out the best course for you. Its free! Code With Harry is my attempt to teach basics and those coding techniques to people in short time which took me ages to learn.</p>
+    </div>
+    <div class=header>
+        <img class="home-img" src="images/home-bg.jpg" alt="">
+    </div>
+</div>
+  </body>
+</html>

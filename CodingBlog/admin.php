2432ac87d9d8a7f415f3f79b4394db94d9814dc4
Deepank <deepankpushpad10@gmail.com>
2022-01-18 23:15:35 +0530

    adding database

---
+++ CodingBlog/admin.php
@@ -0,0 +1,91 @@
+<?php
+
+//function for console log
+function console_log($output, $with_script_tags = true)
+{
+  $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+    ');';
+  if ($with_script_tags) {
+    $js_code = '<script>' . $js_code . '</script>';
+  }
+  echo $js_code;
+}
+
+$servername = "localhost:4306";
+$username = "root";
+$password = "";
+$database = "phub";
+
+//connecting to database
+$conn = mysqli_connect($servername, $username, $password, $database);
+if (!$conn) {
+  console_log("Connection failed " . mysqli_connect_error() . "\n");
+} else {
+  console_log("Connection Successful \n");
+}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $title = $_POST["title"];
+  $description = $_POST["description"];
+  if (empty($title) || empty($description)) {
+    console_log("Please Insert Title and description");
+  }
+  $insertingData = "INSERT INTO `blog` ( `title`, `description`) VALUES ('$title', '$description');";
+  $insert = mysqli_query($conn, $insertingData);
+  if (!$insert) {
+    console_log("Data Insertion Failed " . mysqli_error($conn));
+  } else {
+    console_log("Data Insertion Successful ");
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <script type="text/javascript" src="js/index.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Programming Hub</title>
+</head>
+
+<body>
+
+  <nav id="navbar">
+    <h1><a class="title" href="index.php">Programming Hub</a></h1>
+    <ul>
+      <li><a class="nav-element" href="index.php">Home</a></li>
+      <li><a class="nav-element" href="blog.php">Blog</a></li>
+      <li><a class="nav-element" href="contact.php">Contact</a></li>
+    </ul>
+  </nav>
+
+
+  <div class="home-container">
+    <div class="header-container">
+      <div class="header-content">
+        <h1 class="welcome-line">Create <span class="title">Blog</span></h1>
+
+      </div>
+      <div class=form-div>
+        <form class="form" action='<?php echo $_SERVER['PHP_SELF'] ?>' method="post" >
+          <label for="">Enter Title :</label>
+          <input type="text" class="description-input-1" name="title" id="title" required>
+          <br>
+          <label for="">Enter Description :</label>
+          <input class="description-input-2" type="text" name="description" id="title" required>
+          <br>
+          <input class="read-more-btn" type="submit" value="Submit">
+        </form>
+      </div>
+    </div>
+
+  </div>
+</body>
+
+</html>
--- CodingBlog/blog.php
+++ CodingBlog/blog.php
@@ -3,39 +3,33 @@
 //function for console log
 function console_log($output, $with_script_tags = true)
 {
-    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
-        ');';
-    if ($with_script_tags) {
-        $js_code = '<script>' . $js_code . '</script>';
-    }
-    echo $js_code;
+  $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+    ');';
+  if ($with_script_tags) {
+    $js_code = '<script>' . $js_code . '</script>';
+  }
+  echo $js_code;
 }
 
 //connecting parameters
 $servername = "localhost:4306";
-$username ="root";
-$password ="";
+$username = "root";
+$password = "";
 $database = "phub";
 
 //connecting to database
 $conn = mysqli_connect($servername, $username, $password, $database);
-if(!$conn)
-{
-  console_log("Connection failed ".mysqli_connect_error()."\n");
-}
-else
-{
+if (!$conn) {
+  console_log("Connection failed " . mysqli_connect_error() . "\n");
+} else {
   console_log("Connection Successful \n");
 }
 
 $myDatabase = "CREATE DATABASE phub";
 $dbResult = mysqli_query($conn, $myDatabase);
-if(!$dbResult)
-{
+if (!$dbResult) {
   console_log("Database Creation Failed\n Or Database Exist Already\n");
-}
-else
-{
+} else {
   console_log("Database Creation Successful\n");
 }
 
@@ -43,69 +37,71 @@ $myTable = "CREATE TABLE `blog` (`sno` INT(5) NOT NULL AUTO_INCREMENT, `date` DA
 
 $resultOfTable  = mysqli_query($conn, $myTable);
 if (!$resultOfTable) {
-    console_log("Table Creation Failed " . mysqli_error($conn) . "\n");
-  } else {
-    console_log("\nTable Creation Successful\n");
-  }
-  ?>
+  console_log("Table Creation Failed " . mysqli_error($conn) . "\n");
+} else {
+  console_log("\nTable Creation Successful\n");
+}
+?>
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
   <script type="text/javascript" src="js/index.js"></script>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css" />
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Programing Hub</title> 
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="css/styles.css" />
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Programing Hub</title>
 </head>
+
 <body>
-<nav id="navbar">
-      <h1 ><a class="title" href="index.php" >Programming Hub</a></h1>
-      <ul>
-        <li><a class="nav-element" href="index.php" >Home</a></li>
-        <li><a class="nav-element"  href="blog.php" >Blog</a></li>
-        <li><a class="nav-element"  href="contact.php" >Contact</a></li>
-      </ul>
-    </nav>
-      
-<div class="blog-container">
-    <div class="blog-header-container"> 
-    <div class="blog-content">
-         <h1 class="blog-title"><strong>Programming Hub Blog</strong></h1>
-        </div>    
-   </div>
-   <div class="blogs">
-   <?php 
+  <nav id="navbar">
+    <h1><a class="title" href="index.php">Programming Hub</a></h1>
+    <ul>
+      <li><a class="nav-element" href="index.php">Home</a></li>
+      <li><a class="nav-element" href="blog.php">Blog</a></li>
+      <li><a class="nav-element" href="contact.php">Contact</a></li>
+    </ul>
+  </nav>
+
+  <div class="blog-container">
+    <div class="blog-header-container">
+      <div class="blog-content">
+        <h1 class="blog-title"><strong>Programming Hub Blog</strong></h1>
+      </div>
+    </div>
+    <div class="blogs">
+      <?php
       $detail = "SELECT * FROM `blog`";
       $result = mysqli_query($conn, $detail);
       if (!$result) {
-          console_log(mysqli_error($conn));
+        console_log(mysqli_error($conn));
       }
       $num = mysqli_num_rows($result);
-      if($num>0)
-      {
-        $number =1;
+      if ($num > 0) {
+        $number = 1;
         while ($row = mysqli_fetch_assoc($result)) {
-        echo ' <div class="blog-box">
+          echo ' <div class="blog-box">
         <h4 class="blog-date">Sun Dec 19 2021
 </h4>
-<h2 class="blog-heading">'.$row["title"].'</h2>
-<p class="blog-box-para">'.substr($row["description"],0,100).'<span id="dots">...</span ><span class="readMore" id="more'.$number.'">'.substr($row["description"],100).'</span></p>
-<button class="read-more-btn" onclick="readMore(this.id)" id="myBtn'.$number.'">Read more</button>
+<h2 class="blog-heading">' . $row["title"] . '</h2>
+<p class="blog-box-para">' . substr($row["description"], 0, 100) . '<span id="dots"></span ><span class="readMore" id="more' . $number . '">' . substr($row["description"], 100) . '</span></p>
+<button class="read-more-btn" onclick="readMore(this.id)" id="myBtn' . $number . '">Read more</button>
     </div>';
-    $number=$number+1;
+          $number = $number + 1;
         }
       }
-   
-   ?>
-      </div>
-</div>
 
-
+      ?>
+    </div>
+  </div>
 
 </body>
+
+</body>
+
 </html>

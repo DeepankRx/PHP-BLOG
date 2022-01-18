dad76db30f327b0c8504a5ef4230635fd4332461
Deepank <deepankpushpad10@gmail.com>
2022-01-18 17:24:48 +0530

    second commit

---
+++ CodingBlog/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css" />
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Programming Hub</title>
+  </head>
+  <body>
+    
+    <nav id="navbar">
+      <h1 ><a class="title" href="<?php echo $_SERVER['PHP_SELF'] ?>" >Programming Hub</a></h1>
+      <ul>
+        <li><a class="nav-element" href="<?php echo $_SERVER['PHP_SELF'] ?>" >Home</a></li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+      
+<div class="home-container">
+  <div class="header-container">
+    <div class="header-content">
+        <h1 class="welcome-line"> Welcome To <span class="title">Programming Hub</span></h1>
+        <p class="welcome-text">Confused on which course to take? I have got you covered. Browse courses and find out the best course for you. Its free! Code With Harry is my attempt to teach basics and those coding techniques to people in short time which took me ages to learn.</p>
+    </div>
+    <div class=header>
+        <img class="home-img" src="images/home-bg.jpg" alt="">
+    </div>
+    </div>
+    <div class="recent-blog"><h1>
+        Recent Blogs
+    </h1>
+    <div class="card">
+        <img class="card-img" src="https://picsum.photos/350/200" alt="">
+        <h1 class="card-heading">C/C++</h1>
+        <p class="card-para">Complete Tailwind CSS Course by CodeWithHarry in Hindi - Learn Tailwind CSS from scratch for free!</p>
+    </div>
+    <div class="card">
+        <img class="card-img" src="https://picsum.photos/350/200" alt="">
+        <h1 class="card-heading">Python</h1>
+        <p class="card-para">React is a free and open-source front-end JavaScript library. This series will cover React from starting to the end. We will learn react from the ground up!</p>
+    </div>
+    <div class="card">
+        <img class="card-img" src="https://picsum.photos/350/200" alt="">
+        <h1 class="card-heading">Node Js</h1>
+        <p class="card-para">Data Structure and Algorithm is one of the most crucial things to learn. This course will give you a complete understanding of Data Structure and its implementation.</p>
+    </div>    
+</div>
+</div>
+<footer>
+  <div class="footer">
+    <p class="footer-para">&copy; 2022 Programming Hub</p>
+
+  </div>
+</footer>
+  </body>
+  
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
+
+</html>
